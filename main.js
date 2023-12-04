dc920fee1c3dd19ab29de49bf902c72a6523c405
Mohamed Abdelsameaa <mohamed.abdelsameaa@z2dataeg.com>
2023-12-04 15:49:46 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// let img=document.querySelector('.img')
+// let containeer=document.querySelector('.containeer')
+
+// function phones(src){
+//     img.src=src
+// }
+
+// function changeColor(color){
+//     containeer.style.background=color
+// }
+
+
+let img=document.querySelector('.img')
+let containeer=document.querySelector('.containeer')
+
+function phones(src){
+    img.src=src
+}
+
+
+function changeColor(color){
+    containeer.style.background=color
+}
